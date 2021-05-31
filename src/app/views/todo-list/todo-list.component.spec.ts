c849d87167e3fa7187e94419738acfc4908d1c30
Emanuele Martinelli <emanuele.martinelli@mminformatica.it>
2021-05-31 12:30:06 +0200

    implemented todo list module and some testing

--- src/app/views/todo-list/todo-list.component.spec.ts
+++ src/app/views/todo-list/todo-list.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TodoItem } from '../../modules/shared/models/types';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
@@ -9,10 +10,10 @@ describe('TodoListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ],
-      imports: [HttpClientTestingModule ]
+      declarations: [TodoListComponent],
+      imports: [HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -24,4 +25,15 @@ describe('TodoListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have "items" list populated or empty', () => {
+    expect(component.items).toBeGreaterThanOrEqual(0);
+  })
+
+  it('should not submit the form when it\'s not valid', () => {
+    expect(component.formTodo.valid).toBeFalse();
+    const element: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#submit-btn');
+    expect(element.disabled).toBeTrue();
+  })
+
 });
--- src/app/views/todo-list/todo-list.component.ts
+++ src/app/views/todo-list/todo-list.component.ts
@@ -1,21 +1,133 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { TodoItem } from 'src/app/modules/shared/models/types';
+import Swal from 'sweetalert2';
 import { TodoService } from '../../modules/core/services/todo/todo.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, OnDestroy, AfterViewInit {
 
+  subscriptions: Subscription[] = []
   items: TodoItem[] = [];
+  minDate = moment();
+
+  formTodo: FormGroup = new FormGroup({
+    Id: new FormControl(0),
+    Title: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+    Description: new FormControl('', Validators.maxLength(255)),
+    Due: new FormControl(null),
+    IsComplete: new FormControl(false)
+  });
+
+  //variabili per tabella contenuti
+  columns: string[] = ['Id', 'Title', 'Description', 'Due', 'IsCompleted', 'Actions'];
 
   constructor(private todoService: TodoService) {
-    todoService.getTodo().subscribe(res=>this.items=res)
+
   }
 
+
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.getTodos()
+  }
+
+  //Recupera la lista dei todo
+  getTodos(): void {
+    this.subscriptions.push(
+      this.todoService.getTodo().subscribe(
+        (res) => this.items = res ? res : [],
+        (error) => { console.error(error); Swal.fire("Error", "The application was unable to receive the todos", "error") })
+    );
+  }
+
+  //Registra un nuovo todo se non esiste, altrimenti lo modifica
+  sendTodo(): void {
+    if (!this.formTodo.value.Id) {
+      this.subscriptions.push(
+        this.todoService.insertTodo(this.formTodo.value).subscribe(
+          (result) => {
+            if (result) {
+              Swal.fire("Created", "Todo successfully created", "success")
+            }
+          },
+          (error) => {
+            console.error(error);
+            Swal.fire("Error", "Error occurred creating the todo", "error");
+          },
+          () => {
+            this.getTodos();
+            this.resetForm();
+          }
+        )
+      )
+    }
+    else {
+      this.subscriptions.push(
+        this.todoService.updateTodo(this.formTodo.value).subscribe(
+          () => {
+            Swal.fire("Edited", "Todo successfully edited", "success")
+          },
+          (error) => {
+            console.error(error);
+            Swal.fire("Error", "Error occurred editing the todo", "error");
+          },
+          () => {
+            this.getTodos();
+            this.resetForm();
+          }
+        )
+      )
+    }
+  }
+
+  //Elimina il todo
+  deleteTodo(todo: TodoItem): void {
+    Swal.fire({
+      title: "Conferma",
+      text: `Sei sicuro di eliminare il todo ${todo.Title}?`,
+      showCancelButton: true,
+      cancelButtonText: "Annulla",
+      confirmButtonText: "Elimina Todo"
+    }).then(response => {
+      if (response.isConfirmed) {
+        this.subscriptions.push(
+          this.todoService.deleteTodo(todo.Id).subscribe(
+            (res) => {
+              if (res) {
+                Swal.fire("Deleted", "Todo successfully deleted", "success");
+              }
+            },
+            (error) => {
+              console.error(error);
+              Swal.fire("Error", "Error occurred deleting the todo");
+            },
+            () => this.getTodos()
+          )
+        )
+      }
+    })
+  }
+
+  prepareEditFields(todo: TodoItem) {
+    this.formTodo.setValue(todo);
+  }
+
+  resetForm() {
+    this.formTodo.reset();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
 }
